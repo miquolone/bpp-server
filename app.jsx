d298416dc5ede530a354770a5fcedaeea2ca4a97
miquolone <miquolone@gmail.com>
2022-01-05 00:45:31 +0900

    youtubeがどこまでスクロールしても見える状態を解消させる

--- app.jsx
+++ app.jsx
@@ -36,122 +36,124 @@ function App() {
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet"/>
       </head>
 
-      <body>
-        <div id="backgroundYoutubePlayer"></div>
-        <header>
-          <img class="logo_img" src="/images/bpp_150_alpha.png"/>
-        </header>
+    <body>
+      <div id="backgroundYoutubePlayer"></div>
+      <header>
+        <img class="logo_img" src="/images/bpp_150_alpha.png"/>
+      </header>
 
-        <div class="stickyBackground">
-          ↓これは任意の惑星とかどうかな <br/>
-          <canvas id="myCanvas"></canvas>
-        </div>
+      <div class="stickyBackground">
+        ↓これは任意の惑星とかどうかな <br/>
+        <canvas id="myCanvas"></canvas>
+      </div>
 
-        <main>
-          <h1 class="headerMain">--- 活動を紹介 ---</h1>
+      <main>
+        <h1 class="headerMain">--- 活動を紹介 ---</h1>
 
-          <section class="activity" style={ "padding:unset; margin:unset;" }>
-            <h2>惑星の魔法使い</h2>
-            <p>
-              キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載
-              キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載
-              キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載
-            </p>
-            <div class="activity_contents">
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662291642395918337"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662292741907546113"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662294940930801665"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662296040442429441"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662293841419173889"/>
-            </div>
-          </section>
+        <section class="activity" style={ "padding:unset; margin:unset;" }>
+          <h2>惑星の魔法使い</h2>
+          <p>
+            キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載
+            キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載
+            キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載キャラクタたちの紹介や経緯を記載
+          </p>
+          <div class="activity_contents">
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662291642395918337"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662292741907546113"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662294940930801665"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662296040442429441"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662293841419173889"/>
+          </div>
+        </section>
 
+        <div id="backgroundYoutubePlayerUnVisibler" >&emsp; ここからYoutubeを見えなくしようかな。再生は止めないといけないね</div>
 
-          <section class="activity">
-            <h2>惑星</h2>
-            <p>
-              惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載
-              惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載
-              惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載
-            </p>
-            <div class="activity_contents">
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662286144837779457"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662297139954057217"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662287244349407233"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662288343861035009"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662277348744757249"/>
-              <nft-card vertical="" contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662283945814523905"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662289443372662785"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662290542884290561"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662285045326151681"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662300438488940545"/>
-              <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-              tokenId="68029628652796512442132285988694870032096907384190819011746662298239465684993"/>
-            </div>
-          </section>
+        <section class="activity">
+          <h2>惑星</h2>
+          <p>
+            惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載
+            惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載
+            惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載惑星たちの紹介や経緯を記載
+          </p>
+          <div class="activity_contents">
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662286144837779457"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662297139954057217"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662287244349407233"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662288343861035009"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662277348744757249"/>
+            <nft-card vertical="" contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662283945814523905"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662289443372662785"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662290542884290561"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662285045326151681"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662300438488940545"/>
+            <nft-card vertical contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="68029628652796512442132285988694870032096907384190819011746662298239465684993"/>
+          </div>
+        </section>
 
-          <section class="activity">
-            <h2>音楽</h2>
-            <div class="activity_contents">
-              /// 音楽
-            </div>
-          </section>
+        <section class="activity">
+          <h2>音楽</h2>
+          <div class="activity_contents">
+            /// 音楽
+          </div>
+        </section>
 
-          <section class="activity">
-            <h2>Sandbox</h2>
-            <div class="activity_contents">
-              /// Sandbox
-            </div>
-          </section>
+        <section class="activity">
+          <h2>Sandbox</h2>
+          <div class="activity_contents">
+            /// Sandbox
+          </div>
+        </section>
 
-          <section>
-            <h2>コミュニティメンバ</h2>
-            <div class="flex-center">
-              <div class="memberIcon"></div>
-              <div class="memberIcon"></div>
-              <div class="memberIcon"></div>
-              <div class="memberIcon"></div>
-              <div class="memberIcon"></div>
-            </div>
-          </section>
+        <section>
+          <h2>コミュニティメンバ</h2>
+          <div class="flex-center">
+            <div class="memberIcon"></div>
+            <div class="memberIcon"></div>
+            <div class="memberIcon"></div>
+            <div class="memberIcon"></div>
+            <div class="memberIcon"></div>
+          </div>
+        </section>
 
-          <section class="activity">
-            <h2>BBPについて</h2>
-            <p class="message">
-              ようこそ,Beautiful Planets Projectへ！ <br/>
+        <section class="activity">
+          <h2>BBPについて</h2>
+          <p class="message">
+            ようこそ,Beautiful Planets Projectへ！ <br/>
 
-              私たちは、CG制作の初心者から熟練者までが集まるコミュニティです。<br/>
+            私たちは、CG制作の初心者から熟練者までが集まるコミュニティです。<br/>
 
-              Blenderへの貢献を目的としてCG惑星100個を制作とNFTを公開を進めています。 <br/>
+            Blenderへの貢献を目的としてCG惑星100個を制作とNFTを公開を進めています。 <br/>
 
-              惑星は想像上、架空の惑星でOK！🪐惑星と思えれば良いので、ほとんどルールはありません。（一度うめちゃんがチェックは入れます）
+            惑星は想像上、架空の惑星でOK！🪐惑星と思えれば良いので、ほとんどルールはありません。（一度うめちゃんがチェックは入れます）
 
-              惑星はみんなが集まってくれて活動してくれた証になります。みんなで100個の惑星がつくれたらきっと素晴らしい宝物になるし、そこまでの過程を楽しみたいです。
+            惑星はみんなが集まってくれて活動してくれた証になります。みんなで100個の惑星がつくれたらきっと素晴らしい宝物になるし、そこまでの過程を楽しみたいです。
 
-              もう一つの目標として、このコミュニティで創作をしたいです。既にマスコットキャラクターのパルサーちゃんが誕生しています。そしてその仲間も。みんなこのコミュニティメンバーによって生み出されています。
+            もう一つの目標として、このコミュニティで創作をしたいです。既にマスコットキャラクターのパルサーちゃんが誕生しています。そしてその仲間も。みんなこのコミュニティメンバーによって生み出されています。
 
-              Blenderに限らず、みんなが大好きなこと、得意なことを持ち寄って、新しいことやワクワクすることをやっていきたいです！✨
-            </p>
+            Blenderに限らず、みんなが大好きなこと、得意なことを持ち寄って、新しいことやワクワクすることをやっていきたいです！✨
+          </p>
           <p class={ "corporate_information" }>
             <strong>私たちについて</strong>
             <br/>
             ＊サンプル
             ようこそBeautifulPlanetsProjectへ<br/>
+
             私たちは、CG制作の初心者から熟練者までが集まるコミュニティです。<br/>
             私たちの目的はBlenderへの貢献。目的のためにCG惑星100個の制作とNFTとしてのを公開を進めています。 <br/>
 
@@ -159,56 +161,56 @@ function App() {
             透明性を持たせるためにハッシュの記録を検索し寄付毎に明細を自動で掲載していきます。 <br/>
           </p>
 
-          </section>
+        </section>
 
-        </main>
-        <footer>
-          <a href="https://discord.gg/">Discord招待リンクを検討</a>
-        </footer>
-      </body>
-    </html>
-    );
+      </main>
+      <footer>
+        <a href="https://discord.gg/">Discord招待リンクを検討</a>
+      </footer>
+    </body>
+  </html>
+  );
+  }
+
+
+  async function handler(req) {
+    const { pathname } = new URL( req.url );
+    console.log( pathname );
+
+    if ( pathname.startsWith( "/images/" ) ) {
+      const file = await Deno.readFile( "./assets/" + pathname );
+      return new Response( file, {
+        headers: { "content-type": "application/image" }
+      } );
     }
 
+    if ( pathname.startsWith( "/scripts/" ) ) {
+      const file = await Deno.readFile( "./assets/" + pathname );
+      return new Response( file, {
+        headers: { "content-type": "application/image" }
+      } );
+    }
 
-    async function handler(req) {
-      const { pathname } = new URL( req.url );
-      console.log( pathname );
-
-      if ( pathname.startsWith( "/images/" ) ) {
-        const file = await Deno.readFile( "./assets/" + pathname );
-        return new Response( file, {
-          headers: { "content-type": "application/image" }
-        } );
-      }
-
-      if ( pathname.startsWith( "/scripts/" ) ) {
-        const file = await Deno.readFile( "./assets/" + pathname );
-        return new Response( file, {
-          headers: { "content-type": "application/image" }
-        } );
-      }
-
-      if ( pathname.startsWith( "/styles/" ) ) {
-        const file = await Deno.readFile( "./assets/" + pathname );
-        console.log( "file:::", file );
-        return new Response( file, {
-          headers: {
-            "content-type": "text/css"
-          }
-        } );
-      }
-
-      const html = renderSSR( <App/> );
-      return new Response( '<!DOCTYPE html>' + html, {
+    if ( pathname.startsWith( "/styles/" ) ) {
+      const file = await Deno.readFile( "./assets/" + pathname );
+      console.log( "file:::", file );
+      return new Response( file, {
         headers: {
-          "content-type": "text/html; charset=utf-8"
+          "content-type": "text/css"
         }
       } );
     }
 
-      console.log( "Listening on http://localhost:8000" );
-      serve( handler );
+    const html = renderSSR( <App/> );
+    return new Response( '<!DOCTYPE html>' + html, {
+      headers: {
+        "content-type": "text/html; charset=utf-8"
+      }
+    } );
+  }
+
+    console.log( "Listening on http://localhost:8000" );
+    serve( handler );
 
 
 
