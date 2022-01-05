b43f7afd836571fa280d157936efc13fa82b12d2
miquolone <miquolone@gmail.com>
2022-01-06 00:02:57 +0900

    一部 deno lintの解消

--- assets/scripts/youtube.js
+++ assets/scripts/youtube.js
@@ -1,9 +1,9 @@
 
 
 
-var tag = document.createElement('script');
+const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+const firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 function onYouTubeIframeAPIReady() {
