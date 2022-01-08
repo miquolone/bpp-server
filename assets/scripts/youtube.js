8e4b7cc6877d901afdd47c3ff6c557c8a9d07871
miquolone <miquolone@gmail.com>
2022-01-09 01:50:35 +0900

    youtubeをscope外から操作できるようにして、見えてるとだけ再生にした

--- assets/scripts/youtube.js
+++ assets/scripts/youtube.js
@@ -1,17 +1,20 @@
+  ///document.addEventListener('visibilitychange', e => {console.log(e)}, false);
 const tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
 const firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+let player;
 function onYouTubeIframeAPIReady() {
-  new YT.Player("backgroundYoutubePlayer", {
+  player = new YT.Player("backgroundYoutubePlayer", {
     videoId: "eY58zNPsOJc",
     playerVars: {
       playsinline: 1,
-      loop: 1,
-      listType: "playlist",
-      playlist: "eY58zNPsOJc",
+      loop: 0,
       rel: 0,
+      seekTo: 58,
+      start: 58,
+      end: 60,
       controls: 0,
     },
     events: {
@@ -28,7 +31,45 @@ function onPlayerReady(event) {
 
 function onPlayerHandle(event) {
   if (event.target.getPlayerState() == YT.PlayerState.ENDED) {
+    console.log('完了');
     event.target.mute();
-    event.target.playVideo();
+    event.target.seekTo(58, false);
   }
 }
+
+/**
+ *
+ * playerの停止と再生（0秒から）を操作する
+ * 大画面時にmain髪切れる仕様なので、
+ * スクロール時はカバーが表示されるようにした。
+ *
+ */
+function showVideoPlayer(play) {
+  if (play) {
+    player.playVideo() ;
+  } else {
+    player.stopVideo() 
+  }
+}
+
+globalThis.addEventListener("load", (event) => {
+
+  function intersectionCallback(entries) {
+    entries.forEach(entry => {
+      showVideoPlayer(entry.isIntersecting);
+      if (entry.intersectionRatio >= 0.75) {
+        console.log('isIntersecting', entry.isIntersecting);
+      }
+    });
+  }
+
+  const option = {
+    rootMargin: "10px 2px",
+    threshold: [0, 0.5, 1.0],
+    rootMargin: '-0% 0px'
+  }
+
+  const observer = new IntersectionObserver(intersectionCallback, option);
+  observer.observe(document.querySelector("header"));
+
+}, false);
