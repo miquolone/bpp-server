793b2559e29a7444e4a136f572f7556423297d48
miquolone <miquolone@gmail.com>
2022-01-22 03:04:29 +0900

    starを戻す

--- assets/scripts/palaraxBuilding.js
+++ assets/scripts/palaraxBuilding.js
@@ -6,23 +6,23 @@ globalThis.addEventListener( "load", (event) => {
   document.querySelector( '#overWallCover' ).classList.add( 'hide2s' );
   document.querySelector( '#overWallCover img' ).classList.add( 'hide1s' );
 
-  //const footer = document.querySelector( 'footer' );
-  //[ ...new Array( Math.floor( Math.PI * 10 ) ) ].map( _ => {
-  //  const star = document.createElement( 'img' );
-  //  star.src = "/svg/star001w.svg";
-  //  star.classList.add( 'star' );
-  //  footer.append( star );
-  //} );
+  const footer = document.querySelector( 'footer' );
+  [ ...new Array( Math.floor( Math.PI * 10 ) ) ].map( _ => {
+    const star = document.createElement( 'img' );
+    star.src = "/svg/star001w.svg";
+    star.classList.add( 'star' );
+    footer.append( star );
+  } );
 
-  //let rand30 = Math.random() * 30;
-  //document.querySelectorAll( '.star' ).forEach( node => {
-  //  new simpleParallax( node, {
-  //    delay: rand30,
-  //    orientation: [ 'down', 'up' ][ Math.round( Math.random( 1 ) ) ],
-  //    scale: Math.random() * 520,
-  //    overflow: true
-  //  } );
-  //} );
+  let rand30 = Math.random() * 30;
+  document.querySelectorAll( '.star' ).forEach( node => {
+    new simpleParallax( node, {
+      delay: rand30,
+      orientation: [ 'down', 'up' ][ Math.round( Math.random( 1 ) ) ],
+      scale: Math.random() * 520,
+      overflow: true
+    } );
+  } );
   document.querySelectorAll( [ '.wallUpper', '.wallUnder' ] ).forEach( wall => {
     setTimeout( () => {
       wall.classList.add( 'hide' );
