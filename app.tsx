dc04b17cd0a20cc801e7f7c5b95285580dc7a968
miquolone <miquolone@gmail.com>
2022-01-29 23:57:47 +0900

    test

--- app.tsx
+++ app.tsx
@@ -1,72 +1,12 @@
 import {Application, send, Router} from "https://deno.land/x/oak/mod.ts";
-import {React, ReactDOMServer} from "./dep.ts";
+import {React } from "./dep.ts";
+import ReactDOMServer from "https://jspm.dev/react-dom/server";
 
 import Main from "./Main.tsx";
 
 const BPP: React.FC<{ title: string }> = ({title, children}) => (
     <html className="bg_skew_border">
-        <head>
-            <title>{title}</title>
-            <title>BPP のWeb Ver2</title>
-            <meta name="viewport" content="width=device-width"/>
-            <meta content="#6307c0" name="apple-mobile-web-app-status-bar-style"/>
-            <meta content="#6307c0" name="msapplication-navbutton-color"/>
-            <meta content="#6307c0" name="msapplication-TileColor"/>
-            <meta content="________________________" name="description"/>
-            <meta content="BPP" name="keywords"/>
-            <meta property="og:type" content="WebSite"/>
-            <meta property="og:image" content=""/>
-            <meta content="summary_large_image" name="twitter:card"/>
-            <meta content="@_______" name="twitter:creator"/>
-            <script src="https://unpkg.com/three@0.131.3/build/three.min.js"/>
-            <script src="https://unpkg.com/embeddable-nfts/dist/nft-card.min.js"/>
-
-            <script src="/libs/simpleParallax.min.js"/>
-            <script src="/scripts/threeExample.js"/>
-            <script src="/scripts/youtube.js"/>
-
-            <link rel="icon" href="/images/favicon.png"/>
-            <link href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" rel="stylesheet"/>
-            <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet"/>
-            <link href="/styles/style.css" rel="stylesheet"/>
-
-
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        </head>
-        <body>
-            <div id="overWallCover">
-                <img src="https://cdn.discordapp.com/avatars/578158816680607754/73b2316eabaf54649d6c1fc32fe22a1e.webp?size=240"/>
-            </div>
-
-            <nav className="flex">
-                <a href={"/"}> <img className="logo_img" src="/images/bpp_150_alpha.png"/> </a>
-                <div className={"link"}> | HOME | STORE | SOCIALS |</div>
-            </nav>
-
-            <div id="backgroundYoutubePlayerUnVisibler ">
-                <div id="backgroundYoutubePlayer">&emsp;</div>
-                <div id="youtubeWrap">
-                    <div id="youtubeWrapR">
-                        <div className="wallUpper">&emsp;</div>
-                        <div className="wallUnder">&emsp;</div>
-                    </div>
-                    <div id="youtubeWrapL">
-                        <div className="wallUpper">&emsp;</div>
-                        <div className="wallUnder">&emsp;</div>
-                    </div>
-                </div>
-            </div>
-
-            <header>&emsp;</header>
-
-            {children}
-
-            <footer>
-                &emsp;
-                <script src="/scripts/palaraxBuilding.js"/>
-            </footer>
-        </body>
+        a
     </html>
 );
 
@@ -86,8 +26,13 @@ app.use(async (ctx, next) => {
 });
 
 router.get("/", (context) => {
-    context.response.body = "Hello World!";
+    context.response.body = ReactDOMServer.renderToString(
+        <BPP title="bpp">
+            aaa
+        </BPP>
+    );
 }).get("/image", (ctx) => {
+    ctx.response.body = "Hello World!";
 
 // }).get("/shop/:id", (context) => {
 //     if (context?.params?.id) {
