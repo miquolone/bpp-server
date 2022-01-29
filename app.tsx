4d4b8760246791c28377e9026a81567a1e0944d6
miquolone <miquolone@gmail.com>
2022-01-30 02:11:32 +0900

    Revert "test"
    This reverts commit af4aa744d956ca317f770fda5fd1e8ae11c6e2e1.

--- app.tsx
+++ app.tsx
@@ -2,7 +2,7 @@ import {Application, Router} from "https://deno.land/x/oak/mod.ts";
 import ReactDOMServer from "https://jspm.dev/react-dom/server";
 import React from "https://jspm.dev/react";
 
-const BPP = ({}) => (
+const BPP: React.FC<{ title: string }> = ({title, children}) => (
     <html className="bg_skew_border">
         a
     </html>
@@ -10,6 +10,18 @@ const BPP = ({}) => (
 
 const app = new Application();
 const router = new Router();
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.headers.get("X-Response-Time");
+    console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
 
 router.get("/", (context) => {
     context.response.body = ReactDOMServer.renderToString(
@@ -17,9 +29,22 @@ router.get("/", (context) => {
             aaa
         </BPP>
     );
-})
+}).get("/image", (ctx) => {
+    ctx.response.body = "Hello World!";
+
+// }).get("/shop/:id", (context) => {
+//     if (context?.params?.id) {
+//         context.response.body = context.params.id;
+});
 
 app.use(router.routes());
+// app.use(router.allowedMethods());
+// app.use(async (context) => {
+//     const {pathname} = new URL(context.request.url);
+//     await send(context, "./assets/" + pathname, {
+//         root: `${Deno.cwd()}/`,
+//     });
+// })
 
 await app.listen({port: 8000});
 console.log("Listening on http://localhost:8000");
