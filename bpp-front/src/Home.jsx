e0d82159f92fa0f6d43dfec1ba186426a164ad1e
miquolone <miquolone@gmail.com>
2022-02-01 22:43:43 +0900

    naviをコンポーネント化んで共通化

--- bpp-front/src/Home.jsx
+++ bpp-front/src/Home.jsx
@@ -1,9 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import React from 'react';
 
 function Home() {
   return (
     <>
+      <div id="overWallCover">
+        <img alt="" src="https://cdn.discordapp.com/avatars/578158816680607754/73b2316eabaf54649d6c1fc32fe22a1e.webp?size=240"/>
+      </div>
+
+      <nav className="flex">
+        <a href={ "/" }> <img className="logo_img" src="/assets/images/bpp_150_alpha.png" alt={ "" }/> </a>
+        <div className={ "link" }>
+          |<Link to={ "/" }>HOME </Link>
+          |<Link to={ "/SHOP" }>STORE </Link>
+          |<Link to={ "/SOCIALS " }>SOCIALS </Link> |
+        </div>
+      </nav>
       <Outlet/>
     </>
   );
