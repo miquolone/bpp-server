118a32a9297501cf067ae74e58cbfdcd8b9a2ba8
miquolone <miquolone@gmail.com>
2022-02-01 23:59:37 +0900

    SocialComponentを追加

---
+++ bpp-front/src/SocialComponent.js
@@ -0,0 +1,14 @@
+const SocialComponent = () => {
+
+  return (
+    <>
+
+      <div className="samePlanets">
+        <canvas id="myCanvas">&emsp</canvas>
+      </div>
+
+    </>
+  );
+};
+
+export default SocialComponent;
