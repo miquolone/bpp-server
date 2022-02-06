9b4ff5c38e2a327ed42217fa48985de8bfb16244
miquolone <miquolone@gmail.com>
2022-02-06 22:23:17 +0900

    socialにはダミーデータを置いておく

--- bpp-front/src/SocialComponent.js
+++ bpp-front/src/SocialComponent.js
@@ -15,7 +15,6 @@ const SocialComponent = () => {
 
   return (
     <>
-
       <aside className="circleRotateClassWrap">
         <div className={ circleRotateClassName }/>
         <p className="circleRotateClassWrapInnterText">
@@ -27,6 +26,32 @@ const SocialComponent = () => {
       </aside>
 
 
+      {/*// ダミー いただいている自己紹介を載せていく場所に*/ }
+      <section className="activity">
+        <div className="flex-center">
+          <img src={ "https://cdn.discordapp.com/avatars/578158816680607754/73b2316eabaf54649d6c1fc32fe22a1e.webp?size=240" } alt={ "" }/>
+        </div>
+      </section>
+      <section className="activity">
+        <div className="flex-center">
+          <img src={ "https://cdn.discordapp.com/avatars/578158816680607754/73b2316eabaf54649d6c1fc32fe22a1e.webp?size=240" } alt={ "" }/>
+        </div>
+      </section>
+      <section className="activity">
+        <div className="flex-center">
+          <img src={ "https://cdn.discordapp.com/avatars/578158816680607754/73b2316eabaf54649d6c1fc32fe22a1e.webp?size=240" } alt={ "" }/>
+        </div>
+      </section>
+      <section className="activity">
+        <div className="flex-center">
+          <img src={ "https://cdn.discordapp.com/avatars/578158816680607754/73b2316eabaf54649d6c1fc32fe22a1e.webp?size=240" } alt={ "" }/>
+        </div>
+      </section>
+      <section className="activity">
+        <div className="flex-center">
+          <img src={ "https://cdn.discordapp.com/avatars/578158816680607754/73b2316eabaf54649d6c1fc32fe22a1e.webp?size=240" } alt={ "" }/>
+        </div>
+      </section>
       <section className="activity">
         <div className="flex-center">
           <img src={ "https://cdn.discordapp.com/avatars/578158816680607754/73b2316eabaf54649d6c1fc32fe22a1e.webp?size=240" } alt={ "" }/>
