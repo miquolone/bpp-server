f026ffad6354a09d52b0512809393fe4f5a1fd60
miquolone <miquolone@gmail.com>
2022-02-07 09:38:05 +0900

    denoではコンテンツの検証も使わなくなるので削除

--- server.jsx
+++ server.jsx
@@ -7,28 +7,6 @@
 import { serve } from "https://deno.land/std@0.125.0/http/server.ts";
 
 function HTTPRequestHandler(req) {
-
-  const { pathname } = new URL( req.url );
-  const patternResolve = pathname.match( new RegExp( /.*(\..*)/ ) );
-  if ( patternResolve ) {
-    const [ , extension ] = patternResolve;
-    const mimeList = {
-      ".png": "application/image",
-      ".ico": "application/image",
-      ".svg": "image/svg+xml",
-      ".css": "text/css",
-      ".js": "text/javascript"
-    };
-    try {
-      const file = Deno.readFile( "./bpp-front/public/" + pathname );
-      return new Response( file, {
-        headers: { "content-type": mimeList[ extension ] }
-      } );
-    } catch ( e ) {
-      console.log( 'おやや？' );
-    }
-  }
-
   return new Response( '<!DOCTYPE html><div><a href="https://bpp-ten.vercel.app/">BPPのページは移動しました</a></div>', {
     status: 301,
     statusText: "301 Moved Permanently",
