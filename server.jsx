79f25a833a6b1ccb11b0977e73f6566f9d665c53
miquolone <miquolone@gmail.com>
2022-02-07 09:08:17 +0900

    oakもdeno.comでは動かない予感。 htmlを返すだけのサーバでoakもいらないので削除

--- server.jsx
+++ server.jsx
@@ -4,38 +4,36 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import { serve } from "https://deno.land/std@0.121.0/http/server.ts";
-import { listenAndServe } from "https://deno.land/std@0.121.0/http/mod.ts";
-import {Application, send, Router} from "https://deno.land/x/oak/mod.ts";
-
+import { serve } from "https://deno.land/std@0.125.0/http/server.ts";
+import { listenAndServe } from "https://deno.land/std@0.125.0/http/mod.ts";
 
 async function HTTPRequestHandler(req) {
-    const {pathname} = new URL(req.url);
-    const patternResolve = pathname.match(new RegExp(/.*(\..*)/));
-    if (patternResolve) {
-        const [, extension] = patternResolve;
-        const mimeList = {
-            ".png": "application/image",
-            ".ico": "application/image",
-            ".svg": "image/svg+xml",
-            ".css": "text/css",
-            ".js": "text/javascript"
-        };
-        try {
-            const file = await Deno.readFile("./assets/" + pathname);
-            return new Response(file, {
-                headers: {"content-type": mimeList[extension]}
-            });
-        } catch (e) {
-            console.log('おやや？');
-        }
+  const { pathname } = new URL( req.url );
+  const patternResolve = pathname.match( new RegExp( /.*(\..*)/ ) );
+  if ( patternResolve ) {
+    const [ , extension ] = patternResolve;
+    const mimeList = {
+      ".png": "application/image",
+      ".ico": "application/image",
+      ".svg": "image/svg+xml",
+      ".css": "text/css",
+      ".js": "text/javascript"
+    };
+    try {
+      const file = await Deno.readFile( "./assets/" + pathname );
+      return new Response( file, {
+        headers: { "content-type": mimeList[ extension ] }
+      } );
+    } catch ( e ) {
+      console.log( 'おやや？' );
     }
+  }
 
-    const html = ReactDOMServer.renderToString(<Main/>);
-    return new Response(html, {
-        headers: {"content-type": "text/html; charset=utf-8"}
-    });
+  const html = ReactDOMServer.renderToString( <Main/> );
+  return new Response( html, {
+    headers: { "content-type": "text/html; charset=utf-8" }
+  } );
 }
 
-console.log("Listening on http://localhost:8000");
-serve(HTTPRequestHandler);
+console.log( "Listening on http://localhost:8000" );
+serve( HTTPRequestHandler );
