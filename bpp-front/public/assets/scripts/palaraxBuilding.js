7f855b42aef719e33a54f4de232d266b17c8b84e
miquolone <miquolone@gmail.com>
2022-02-12 04:55:01 +0900

    ある程度スクロールすると、YOutubeはpip表示になる

--- bpp-front/public/assets/scripts/palaraxBuilding.js
+++ bpp-front/public/assets/scripts/palaraxBuilding.js
@@ -2,4 +2,27 @@ window.addEventListener( "load", () => {
   console.log( '[+] BPP init' );
   document.querySelector( '#overWallCover' )?.classList.add( 'hide' );
   document.querySelector( '#overWallCover img' )?.classList.add( 'hide1s' );
-} );
+
+
+  /// Youtubeわく専用 動画はpip で表示する
+  function intersectionCallback(entries) {
+    entries.forEach( entry => {
+
+      if ( !entry.isIntersecting ) {
+        document.querySelector( '#backgroundYoutubePlayer' ).classList.add( 'backgroundYoutubePlayerScrolled' );
+      } else {
+        document.querySelector( '#backgroundYoutubePlayer' ).classList.remove( 'backgroundYoutubePlayerScrolled' );
+      }
+
+      console.log( '変更した', entry.isIntersecting );
+    } );
+  }
+
+  const observer = new IntersectionObserver( intersectionCallback, { threshold: [ 0, 0.01, 1.0 ], rootMargin: '0% 0px' } );
+  observer.observe( document.querySelector( "header" ) );
+
+  document.addEventListener( 'visibilitychange', e => {console.log( e );}, false );
+
+  console.log( 'Observer復帰' );
+}, false );
+
