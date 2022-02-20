4628da67b29eb7bad9e45768f084bd63fe1ee373
miquolone <miquolone@gmail.com>
2022-02-20 14:42:52 +0900

    滞在時間はeventに存在している値を利用。

--- bpp-front/public/assets/scripts/coupon.js
+++ bpp-front/public/assets/scripts/coupon.js
@@ -6,13 +6,17 @@ window.addEventListener( "load", () => {
     count.pageMove++;
     count.currentLoad = new Date().getTime();
     count.timeDiff = count.currentLoad - count.initialLoad;
+    if ( count.pageMove % 2 === 0 ) {
+      // console.log( count.pageMove / 2, "回目の確認" );
+    }
     // console.log( count, e );
+
     // １時間経過していたら 特別クーポンを出す　←　なにの？
-    if ( count.timeDiff > 3600 * 1000 && count.show <= 3600 * 1000 ) {
+    if ( e.timeStamp > 3600 * 1000 && count.show <= 3600 * 1000 ) {
       count.show = 30 * 1000 + 1;
       alert( '１時間クーポンが登場しました' );
     }
-    if ( count.timeDiff > 89600 * 10 * 1000 && count.show <= 89600 * 10 * 1000 ) {
+    if ( e.timeStamp > 89600 * 10 * 1000 && count.show <= 89600 * 10 * 1000 ) {
       alert( '２４０時間クーポンが登場しました' );
       count.show = 89600 * 10 * 1000 + 1;
     }
