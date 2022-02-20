1b6a169eb903a5542c6ac8aa84fea01fe907c7b4
miquolone <miquolone@gmail.com>
2022-02-20 17:10:14 +0900

    擬似ローディング

--- bpp-front/src/components/SplashWindowComponent.jsx
+++ bpp-front/src/components/SplashWindowComponent.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 
 const SplashWindowComponent = () => {
 
-  const SplashCharacter = styled.img( props => ( { background: props.background } ) );
   const LoadingParcentage = styled.p( props => ( {
     fontSize: "1.2rem"
   } ) );
@@ -13,10 +12,10 @@ const SplashWindowComponent = () => {
 
   useEffect( () => {
     console.log( '[+] Splash init' );
-    document.querySelector( '#overWallCover #step2 img' )?.classList.add( 'hide2s' );
     setTimeout( () => {
+      document.querySelector( '#overWallCoverInner img' ).classList.add( 'hide2s' );
       document.querySelector( '#overWallCover' ).classList.add( 'hide1s' );
-    }, 3000 );
+    }, 1000 );
 
   }, [] );
 
@@ -24,8 +23,7 @@ const SplashWindowComponent = () => {
     <>
       <div id="overWallCover">
         <div id="overWallCoverInner">
-          <SplashCharacter alt="" src="https://media.discordapp.net/attachments/932636979374477362/943141661255561256/PULSACHAN_alpha.png"/>
-          {/*<SplashCharacter alt="" src="https://cdn.discordapp.com/icons/895078439709708318/ef2465a8d14d0f299ad248bf70c72bbe.webp"/>*/ }
+          <img alt="" src="https://media.discordapp.net/attachments/932636979374477362/943141661255561256/PULSACHAN_alpha.png"/>
           <LoadingParcentage className={ "loadingPercentage" }>100%</LoadingParcentage>
           <CharacterAuthor className={ "splashCreator" }>BPP-Attender authored @De</CharacterAuthor>
         </div>
