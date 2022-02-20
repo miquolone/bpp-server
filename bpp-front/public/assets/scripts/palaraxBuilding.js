60a6af358e18fe169fbd67149889356a48d32eb3
miquolone <miquolone@gmail.com>
2022-02-20 14:02:08 +0900

    タブを落とさずにいるとクーポンが・・・

--- bpp-front/public/assets/scripts/palaraxBuilding.js
+++ bpp-front/public/assets/scripts/palaraxBuilding.js
@@ -1,4 +1,22 @@
+
+// (10日の間に20回、タブを残していたら何かする)
+console.log( '[+] coupon init 10日間閉じなければクーポンが！！！←なにの' );
+const count = { pageMove: 0, initialLoad: new Date().getTime(), show: 0 };
 window.addEventListener( "load", () => {
-  document.addEventListener( 'visibilitychange', e => {console.log( e );}, false );
+  document.addEventListener( 'visibilitychange', e => {
+    count.pageMove++;
+    count.currentLoad = new Date().getTime();
+    count.timeDiff = count.currentLoad - count.initialLoad;
+    // console.log( count, e );
+    // １時間経過していたら 特別クーポンを出す　←　なにの？
+    if ( count.timeDiff > 30 && count.show <= 30 ) {
+      count.show = 31;
+      alert( '１時間クーポンが登場しました' );
+    }
+    if ( count.timeDiff > 89600 * 10 && count.show <= 89600 * 10 ) {
+      alert( '２４０時間クーポンが登場しました' );
+      count.show = 89601;
+    }
+  }, false );
 }, false );
 
