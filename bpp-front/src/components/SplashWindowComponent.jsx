ec59cc622f2127c47cebacd6c0bc396d2a574712
miquolone <miquolone@gmail.com>
2022-02-20 16:24:29 +0900

    無意味なスタイルドコンポーネントを入れてみよう。　無駄

--- bpp-front/src/components/SplashWindowComponent.jsx
+++ bpp-front/src/components/SplashWindowComponent.jsx
@@ -1,10 +1,19 @@
 import { useEffect } from 'react';
+import styled from 'styled-components';
 
 const SplashWindowComponent = () => {
+
+  const SplashCharacter = styled.img( props => ( { background: props.background } ) );
+  const LoadingParcentage = styled.p( props => ( {
+    fontSize: "1.2rem"
+  } ) );
+  const CharacterAuthor = styled.p( props => ( {
+    fontSize: "1rem"
+  } ) );
+
   useEffect( () => {
     console.log( '[+] Splash init' );
-
-    document.querySelector( '#overWallCover img' )?.classList.add( 'hide1s' );
+    document.querySelector( '#overWallCover #step2 img' )?.classList.add( 'hide2s' );
     setTimeout( () => {
       document.querySelector( '#overWallCover' ).classList.add( 'hide1s' );
     }, 3000 );
@@ -15,9 +24,10 @@ const SplashWindowComponent = () => {
     <>
       <div id="overWallCover">
         <div id="overWallCoverInner">
-          <img alt="" src="https://media.discordapp.net/attachments/932636979374477362/943141661255561256/PULSACHAN_alpha.png"/>
-          <p className={ "loadingPercentage" }>100%</p>
-          <p className={ "splashCreator" }>@De</p>
+          <SplashCharacter alt="" src="https://media.discordapp.net/attachments/932636979374477362/943141661255561256/PULSACHAN_alpha.png"/>
+          {/*<SplashCharacter alt="" src="https://cdn.discordapp.com/icons/895078439709708318/ef2465a8d14d0f299ad248bf70c72bbe.webp"/>*/ }
+          <LoadingParcentage className={ "loadingPercentage" }>100%</LoadingParcentage>
+          <CharacterAuthor className={ "splashCreator" }>BPP-Attender authored @De</CharacterAuthor>
         </div>
       </div>
     </>
