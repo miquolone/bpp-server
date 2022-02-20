3510b771e2c700c0bcbf6fd59a0b057b6096f115
miquolone <miquolone@gmail.com>
2022-02-20 14:09:38 +0900

    couponの時間軸を修正

--- bpp-front/public/assets/scripts/coupon.js
+++ bpp-front/public/assets/scripts/coupon.js
@@ -1,4 +1,3 @@
-
 // (10日の間に20回、タブを残していたら何かする)
 console.log( '[+] coupon init 10日間閉じなければクーポンが！！！←なにの' );
 const count = { pageMove: 0, initialLoad: new Date().getTime(), show: 0 };
@@ -9,13 +8,13 @@ window.addEventListener( "load", () => {
     count.timeDiff = count.currentLoad - count.initialLoad;
     // console.log( count, e );
     // １時間経過していたら 特別クーポンを出す　←　なにの？
-    if ( count.timeDiff > 30 && count.show <= 30 ) {
-      count.show = 31;
+    if ( count.timeDiff > 3600 * 1000 && count.show <= 3600 * 1000 ) {
+      count.show = 30 * 1000 + 1;
       alert( '１時間クーポンが登場しました' );
     }
-    if ( count.timeDiff > 89600 * 10 && count.show <= 89600 * 10 ) {
+    if ( count.timeDiff > 89600 * 10 * 1000 && count.show <= 89600 * 10 * 1000 ) {
       alert( '２４０時間クーポンが登場しました' );
-      count.show = 89601;
+      count.show = 89600 * 10 * 1000 + 1;
     }
   }, false );
 }, false );
