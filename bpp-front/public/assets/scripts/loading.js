5921269341b6804c3b217feec39d4d6610bf3bc4
miquolone <miquolone@gmail.com>
2022-02-21 01:50:42 +0900

    lodingをsplashが存在するときのみに限定

--- bpp-front/public/assets/scripts/loading.js
+++ bpp-front/public/assets/scripts/loading.js
@@ -1,10 +1,12 @@
-// window.addEventListener( "load", () => {
-//   let count = 0;
-//   const target = document.querySelector( '.loadingPercentage' );
-//   window.nnn = setInterval( () => {
-//     target.innerHTML = count + "%";
-//     if ( ++count > 100 ) {
-//       clearInterval( window.nnn );
-//     }
-//   }, 1 );
-// } );
+window.addEventListener( "load", () => {
+  let count = 0;
+  const target = document.querySelector( '.loadingPercentage' );
+  if ( target ) {
+    window.nnn = setInterval( () => {
+      target.innerHTML = count + "%";
+      if ( ++count > 100 ) {
+        clearInterval( window.nnn );
+      }
+    }, 1 );
+  }
+} );
