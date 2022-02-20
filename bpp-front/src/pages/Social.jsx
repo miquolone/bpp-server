f951c418c809b6d298dafd0328f3d71cc3551953
miquolone <miquolone@gmail.com>
2022-02-20 14:06:51 +0900

    fix html lint

--- bpp-front/src/pages/Social.jsx
+++ bpp-front/src/pages/Social.jsx
@@ -17,12 +17,12 @@ const Social = () => {
     <>
       <aside className="circleRotateClassWrap">
         <div className={ circleRotateClassName }/>
-        <p className="circleRotateClassWrapInnterText">
+        <div className="circleRotateClassWrapInnterText">
           <div className="samePlanets">
             <canvas id="myCanvas">&emsp</canvas>
           </div>
           S.O.C.I.A.L
-        </p>
+        </div>
       </aside>
 
       <header>&emsp;
