0fa5e88bb2bef138af9a10b148a02639b434af6b
miquolone <miquolone@gmail.com>
2022-02-27 22:55:27 +0900

    socialの枠にコンテンツ配置を可能にする

--- bpp-front/src/components/wakuComponent.jsx
+++ bpp-front/src/components/wakuComponent.jsx
@@ -1,8 +1,22 @@
 const WakuComponent = () => {
   return (
     <>
-      <div Style={ "min-width:200px;max-width:300px;width: 100%;" }>
-        <img src={ '/assets/svg/waku.svg' } alt={ "" }/>
+      <div Style={
+        "background: url(/assets/svg/waku.svg) no-repeat;\n" +
+        "height: 186px;\n" +
+        "width: 326px;"
+      }>
+        <div Style={ "height:38%; background:#777a; display:flex;" }>
+          <div Style={ "width:25%; background:#872a;" }>
+            <img Style={ "background:#fff; width:60px; margin:10px;border-radius:2rem;" } src={ '/assets/images/ume.webp' }/></div>
+          <div
+            Style={ "width:75%; padding:17px 40px 13px; color:#eee; font-size:0.9rem;background:#d723;word-break:break-all;line-height:1rem;" }>BBBBBBBBBBBBBBBBBBBBB
+          </div>
+        </div>
+        <div Style={ "height:62%; background:#777a" }>
+          <p
+            Style={ "margin:0 auto;max-height:3rem;overflow:hidden;padding:12px; color:#eee; font-size:0.9rem;background:#d723;word-break:break-all;line-height:1.3rem;" }>BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB</p>
+        </div>
       </div>
     </>
   );
