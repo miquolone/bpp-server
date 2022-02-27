2ff6c0e9f54ab8364960a77b48fa208bdea5e548
miquolone <miquolone@gmail.com>
2022-02-27 23:21:52 +0900

    styleを分離

--- bpp-front/src/components/wakuComponent.jsx
+++ bpp-front/src/components/wakuComponent.jsx
@@ -1,21 +1,14 @@
 const WakuComponent = () => {
   return (
     <>
-      <div Style={
-        "background: url(/assets/svg/waku.svg) no-repeat;\n" +
-        "height: 186px;\n" +
-        "width: 326px;"
-      }>
-        <div Style={ "height:38%; background:#777a; display:flex;" }>
-          <div Style={ "width:25%; background:#872a;" }>
-            <img Style={ "background:#fff; width:60px; margin:10px;border-radius:2rem;" } src={ '/assets/images/ume.webp' }/></div>
-          <div
-            Style={ "width:75%; padding:17px 40px 13px; color:#eee; font-size:0.9rem;background:#d723;word-break:break-all;line-height:1rem;" }>BBBBBBBBBBBBBBBBBBBBB
-          </div>
+      <div className={ "gg" }>
+        <div className={ "aa" }>
+          <div className={ "bb" }>
+            <img className={ "cc" } src={ '/assets/images/ume.webp' }/></div>
+          <div className={ "dd" }>BBBBBBBBBBBBBBBBBBBBB</div>
         </div>
-        <div Style={ "height:62%; background:#777a" }>
-          <p
-            Style={ "margin:0 auto;max-height:3rem;overflow:hidden;padding:12px; color:#eee; font-size:0.9rem;background:#d723;word-break:break-all;line-height:1.3rem;" }>BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB</p>
+        <div className={ "ee" }>
+          <p className={ "ff" }>BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB</p>
         </div>
       </div>
     </>
