b8e8b01eecea4043fd267dad8084936777d75f6d
miquolone <miquolone@gmail.com>
2022-03-01 23:35:49 +0900

    testが通る状態に修正

--- bpp-front/src/App.test.js
+++ bpp-front/src/App.test.js
@@ -1,9 +1,16 @@
-
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+window.particleInit = jest.fn();
+
+test( 'intersectionObserver has called', () => {
+
+  const observe = jest.fn();
+  window.IntersectionObserver = jest.fn( () => ( {
+    observe
+  } ) );
+
+  render( <App/> );
+  expect( observe ).toHaveBeenCalled();
+} );
