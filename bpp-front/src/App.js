918eed58c1b650db7585f95c74a6cf752496a5e9
miquolone <miquolone@gmail.com>
2022-03-01 23:35:00 +0900

    BrowserRouterをAppに移動

--- bpp-front/src/App.js
+++ bpp-front/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import React from 'react';
 import Home from './pages/Home';
@@ -11,26 +11,28 @@ import OrganizeComponent from './pages/Organize';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Home/> }>
-        <Route index element={ <MainComponent/> }/>
-        <Route path="store" element={ <StoreComponent/> }>
-          <Route path=":nftId" element={ <StoreComponent/> }/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home/> }>
+          <Route index element={ <MainComponent/> }/>
+          <Route path="store" element={ <StoreComponent/> }>
+            <Route path=":nftId" element={ <StoreComponent/> }/>
+          </Route>
+          <Route path="social" element={ <SocialComponent/> }>
+            <Route path=":name" element={ <SocialComponent/> }/>
+          </Route>
+          <Route path="transaction" element={ <TransactionComponent/> }>
+            <Route path=":hash" element={ <TransactionComponent/> }/>
+          </Route>
+          <Route path="organize" element={ <OrganizeComponent/> }>
+            <Route path=":hash" element={ <OrganizeComponent/> }/>
+          </Route>
+          <Route path="contact" element={ <ContactComponent/> }>
+            <Route path=":hash" element={ <ContactComponent/> }/>
+          </Route>
         </Route>
-        <Route path="social" element={ <SocialComponent/> }>
-          <Route path=":name" element={ <SocialComponent/> }/>
-        </Route>
-        <Route path="transaction" element={ <TransactionComponent/> }>
-          <Route path=":hash" element={ <TransactionComponent/> }/>
-        </Route>
-        <Route path="organize" element={ <OrganizeComponent/> }>
-          <Route path=":hash" element={ <OrganizeComponent/> }/>
-        </Route>
-        <Route path="contact" element={ <ContactComponent/> }>
-          <Route path=":hash" element={ <ContactComponent/> }/>
-        </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- bpp-front/src/index.jsx
+++ bpp-front/src/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
 
 const BPPROOT = document.getElementById( 'root' );
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>,
   BPPROOT
 );
