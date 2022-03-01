06e8f1aa2103c03f4cf9e56b66a97bb6f1717e1d
miquolone <miquolone@gmail.com>
2022-03-02 00:31:49 +0900

    airtable格納メンバのかずなど、jsetを拡張

--- bpp-front/src/App.test.js
+++ bpp-front/src/App.test.js
@@ -1,16 +1,50 @@
-import { render, screen } from '@testing-library/react';
+import {shallow, render, screen } from '@testing-library/react';
+import { table, minifyRecords } from './lib/airtable';
 
 import App from './App';
 
+
 window.particleInit = jest.fn();
 
-test( 'intersectionObserver has called', () => {
+it("test 1", () => { });
 
-  const observe = jest.fn();
-  window.IntersectionObserver = jest.fn( () => ( {
-    observe
-  } ) );
+describe("#IntersectionObserver", () => {
 
-  render( <App/> );
-  expect( observe ).toHaveBeenCalled();
+  test( 'intersectionObserver has called', () => {
+    const observe = jest.fn();
+    window.IntersectionObserver = jest.fn( () => ( {
+      observe
+    } ) );
+
+    render( <App/> );
+    expect( observe ).toHaveBeenCalled();
+  } );
 } );
+
+describe("#spyOn", () => {
+
+  test('spy result inspection Math.random return 0.1', () => {
+    const spy = jest.spyOn(Math, "random").mockImplementation(() => 0.1);
+    expect(Math.random()).toBe(0.1);
+    spy.mockRestore();
+  });
+
+  it("reset spy Math.random not return 0.1", () => {
+    expect(Math.random()).toBeGreaterThan(0.1);
+  });
+
+  it("Math.random return under 1", () => {
+    expect(Math.random() < 1).toEqual(true);
+  });
+
+});
+
+describe("#AirTable", () => {
+
+  test('airtable inerted member size more than 12', async ()=> {
+    const records = await table.select( {} ).all();
+    const minifiedRecords = await minifyRecords( records );
+    expect(records.length).toBeGreaterThan(12)
+  })
+})
+
