ec8937f9d56647480c2a6ab3b7a2c820e604c896
miquolone <miquolone@gmail.com>
2022-03-04 01:54:18 +0900

    splashを100%露出

--- bpp-front/src/components/SplashWindowComponent.jsx
+++ bpp-front/src/components/SplashWindowComponent.jsx
@@ -16,7 +16,7 @@ const splashedAtom = atomWithStorage(
 const SplashWindowComponent = () => {
   const [ splashed, setSplashed ] = useAtom( splashedAtom );
   useEffect( () => {
-    // if ( !splashed ) {
+    if ( !splashed ) {
       console.log( '[+] Splash init' );
       document.querySelector( '#overWallCoverInner' ).classList.add( 'expand1s' );
       document.querySelector( '#overWallCoverInner' ).style.backgroundColor = '#000';
@@ -27,9 +27,8 @@ const SplashWindowComponent = () => {
         document.querySelector( '#overWallCoverInner img' ).classList.add( 'hide2s' );
         document.querySelector( '#overWallCover' ).classList.add( 'hide2s' );
         // setSplashed( true );
-        setSplashed( true );
       }, 3000 );
-    // } // splash の露出を100%にする
+    }
   }, [ setSplashed, splashed ] );
 
   return (
