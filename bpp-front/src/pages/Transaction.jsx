e42e157e1807a06395d7a1c8151823f89f663e11
miquolone <miquolone@gmail.com>
2022-03-09 19:51:23 +0900

    寄付歴を正しい1個のみにする

--- bpp-front/src/pages/Transaction.jsx
+++ bpp-front/src/pages/Transaction.jsx
@@ -25,10 +25,9 @@ const Transaction = () => {
         </div>
       </aside>
 
-        <div id="particles-js"/>
+      <div id="particles-js"/>
 
-      <div id={"main"}>
-        {/*// ダミー トークンの移動履歴を載せていく場所に*/ }
+      <div id={ "main" }>
         <section className="activity">
           <div className="flex-center">
             <ul>
@@ -44,77 +43,6 @@ const Transaction = () => {
               </ol>
             </ul>
           </div>
-          <div className="flex-center">
-            <ul>
-              <ol>
-                From: 0x966767c24782dda01b3c22fc60c83ca1d3a356a2
-              </ol>
-              <ol>
-                To: 0x9de8991c56f622175274fb358f981af6f903a799
-              </ol>
-              <ol>
-                <a target="_blank" rel="noreferrer"
-                   href="https://etherscan.io/tx/0x68bce054b5e4ac72ce8ec2c96120792ed8a911204e8c0ee32785bc6b1956827c">EthScan（外部サイト）</a>
-              </ol>
-            </ul>
-          </div>
-          <div className="flex-center">
-            <ul>
-              <ol>
-                From: 0x966767c24782dda01b3c22fc60c83ca1d3a356a2
-              </ol>
-              <ol>
-                To: 0x9de8991c56f622175274fb358f981af6f903a799
-              </ol>
-              <ol>
-                <a target="_blank" rel="noreferrer"
-                   href="https://etherscan.io/tx/0x68bce054b5e4ac72ce8ec2c96120792ed8a911204e8c0ee32785bc6b1956827c">EthScan（外部サイト）</a>
-              </ol>
-            </ul>
-          </div>
-          <div className="flex-center">
-            <ul>
-              <ol>
-                From: 0x966767c24782dda01b3c22fc60c83ca1d3a356a2
-              </ol>
-              <ol>
-                To: 0x9de8991c56f622175274fb358f981af6f903a799
-              </ol>
-              <ol>
-                <a target="_blank" rel="noreferrer"
-                   href="https://etherscan.io/tx/0x68bce054b5e4ac72ce8ec2c96120792ed8a911204e8c0ee32785bc6b1956827c">EthScan（外部サイト）</a>
-              </ol>
-            </ul>
-          </div>
-          <div className="flex-center">
-            <ul>
-              <ol>
-                From: 0x966767c24782dda01b3c22fc60c83ca1d3a356a2
-              </ol>
-              <ol>
-                To: 0x9de8991c56f622175274fb358f981af6f903a799
-              </ol>
-              <ol>
-                <a target="_blank" rel="noreferrer"
-                   href="https://etherscan.io/tx/0x68bce054b5e4ac72ce8ec2c96120792ed8a911204e8c0ee32785bc6b1956827c">EthScan（外部サイト）</a>
-              </ol>
-            </ul>
-          </div>
-          <div className="flex-center">
-            <ul>
-              <ol>
-                From: 0x966767c24782dda01b3c22fc60c83ca1d3a356a2
-              </ol>
-              <ol>
-                To: 0x9de8991c56f622175274fb358f981af6f903a799
-              </ol>
-              <ol>
-                <a target="_blank" rel="noreferrer"
-                   href="https://etherscan.io/tx/0x68bce054b5e4ac72ce8ec2c96120792ed8a911204e8c0ee32785bc6b1956827c">EthScan（外部サイト）</a>
-              </ol>
-            </ul>
-          </div>
-
         </section>
       </div>
     </>
