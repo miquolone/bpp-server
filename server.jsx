b63655397de537c3b218db5eb8255941d07a6a7c
miquolone <miquolone@gmail.com>
2022-03-12 18:40:34 +0900

    serverを追加

---
+++ server.jsx
@@ -0,0 +1,20 @@
+/** @jsx h */
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import { serve } from "https://deno.land/std@0.125.0/http/server.ts";
+
+function HTTPRequestHandler(req) {
+  return new Response( '', {
+    status: 301,
+    statusText: "301 Moved Permanently",
+    headers: {
+      "Location": "https://bpp-ten.vercel.app/"
+    }
+  } );
+}
+
+console.log( "Listening on http://localhost:8000" );
+await serve( HTTPRequestHandler );
