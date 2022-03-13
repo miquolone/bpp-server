5596b28f557d3c899962e40c3a2c9bf6c33f61e5
miquolone <miquolone@gmail.com>
2022-03-13 14:43:06 +0900

    app.tsx back to 9939ee8

--- app.tsx
+++ app.tsx
@@ -1,50 +1,42 @@
 
-import { serve } from "https://deno.land/std@0.125.0/http/server.ts";
-//import {Application, send, Router} from "https://deno.land/x/oak/mod.ts";
-//
-//import Main from "./Main.tsx";
-//
-//const app = new Application();
-//const router = new Router();
-//
-//router.get("/", (context) => {
-//  context.response.redirect("https://bpp-ten.vercel.app/")
-//});
-//
-//router.get("/next", (context) => {
-//})
-//
-//router.get("/image", () => { })
-//
-//router.get("/shop/:id", (context) => {
-//  if (context?.params?.id) {
-//    context.response.body = context.params.id;
-//  }
-//});
-//
-//app.use(router.routes());
-//app.use(router.allowedMethods());
-//app.use(async (context) => {
-//  console.log(context.request.url)
-//  const {pathname} = new URL(context.request.url.pathname);
-//  await send(context, "./assets/" + pathname, {
-//    root: `${Deno.cwd()}/`,
-//  });
-//})
-//
-//
-//await app.listen({port: 8000});
-//console.log("Listening on http://localhost:8000");
+import { serve } from "https://deno.land/std@0.129.0/http/server.ts";
+import {Application, send, Router} from "https://deno.land/x/oak/mod.ts";
+import {React, ReactDOMServer} from "./dep.ts";
 
-function HTTPRequestHandler() {
-  return new Response( '', {
-    status: 301,
-    statusText: "301 Moved Permanently",
-    headers: {
-      "Location": "https://bpp-ten.vercel.app/"
-    }
-  } );
-}
+import Main from "./Main.tsx";
 
-console.log( "Listening on http://localhost:8000" );
-await serve( HTTPRequestHandler );
+const app = new Application();
+const router = new Router();
+
+router.get("/", (context) => {
+  context.response.redirect("https://bpp-ten.vercel.app/")
+});
+
+router.get("/next", (context) => {
+  // deno.landでは稼働しない
+  context.response.body = ReactDOMServer.renderToString(
+    <Main/>
+  );
+})
+
+router.get("/image", () => { })
+
+router.get("/shop/:id", (context) => {
+  if (context?.params?.id) {
+    context.response.body = context.params.id;
+  }
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(async (context) => {
+  console.log(context.request.url)
+  const {pathname} = new URL(context.request.url.pathname);
+  await send(context, "./assets/" + pathname, {
+    root: `${Deno.cwd()}/`,
+  });
+})
+
+
+await app.listen({port: 8000});
+console.log("Listening on http://localhost:8000");
