3d903ff2af108888f3059590662b67a8949fa0c7
miquolone <miquolone@gmail.com>
2022-03-13 14:30:04 +0900

    tes

--- app.tsx
+++ app.tsx
@@ -1,40 +1,40 @@
 
-import { serve } from "https://deno.land/std@0.125.0/http/server.ts";
-import {Application, send, Router} from "https://deno.land/x/oak/mod.ts";
-
-import Main from "./Main.tsx";
-
-const app = new Application();
-const router = new Router();
-
-router.get("/", (context) => {
-  context.response.redirect("https://bpp-ten.vercel.app/")
-});
-
-router.get("/next", (context) => {
-})
-
-router.get("/image", () => { })
-
-router.get("/shop/:id", (context) => {
-  if (context?.params?.id) {
-    context.response.body = context.params.id;
-  }
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.use(async (context) => {
-  console.log(context.request.url)
-  const {pathname} = new URL(context.request.url.pathname);
-  await send(context, "./assets/" + pathname, {
-    root: `${Deno.cwd()}/`,
-  });
-})
-
-
-await app.listen({port: 8000});
-console.log("Listening on http://localhost:8000");
+import { serve } from "https://deno.land/std@0.129.0/http/server.ts";
+//import {Application, send, Router} from "https://deno.land/x/oak/mod.ts";
+//
+//import Main from "./Main.tsx";
+//
+//const app = new Application();
+//const router = new Router();
+//
+//router.get("/", (context) => {
+//  context.response.redirect("https://bpp-ten.vercel.app/")
+//});
+//
+//router.get("/next", (context) => {
+//})
+//
+//router.get("/image", () => { })
+//
+//router.get("/shop/:id", (context) => {
+//  if (context?.params?.id) {
+//    context.response.body = context.params.id;
+//  }
+//});
+//
+//app.use(router.routes());
+//app.use(router.allowedMethods());
+//app.use(async (context) => {
+//  console.log(context.request.url)
+//  const {pathname} = new URL(context.request.url.pathname);
+//  await send(context, "./assets/" + pathname, {
+//    root: `${Deno.cwd()}/`,
+//  });
+//})
+//
+//
+//await app.listen({port: 8000});
+//console.log("Listening on http://localhost:8000");
 
 function HTTPRequestHandler() {
   return new Response( '', {
